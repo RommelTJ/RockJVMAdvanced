7b58a2d5e84173b65d965c17db310b66b21b9073
Rommel Rico <me@rommelrico.com>
2019-02-06 06:49:16 -0800

    Syntax Sugar #1: Methods with single param

--- src/lectures/part1/DarkSugars.scala
+++ src/lectures/part1/DarkSugars.scala
@@ -1,5 +1,23 @@
 package lectures.part1
 
+import scala.util.Try
+
 object DarkSugars extends App {
+
+  // Syntax Sugar #1: Methods with single param
+  def singleArgMethod(arg: Int): String = s"$arg little ducks..."
+  val description = singleArgMethod {
+    // Write some code
+    42
+  }
+  println(description) // 42 little ducks...
+
+  val aTryInstance = Try { // Java's try {...}
+    throw new RuntimeException
+  }
+
+  List(1,2,3).map { x =>
+    x + 1
+  }
 
 }
