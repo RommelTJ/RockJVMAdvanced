5b497501758ccd8b21526eb446bbdc26a1c1d4ea
Rommel Rico <me@rommelrico.com>
2019-02-07 20:42:49 -0800

    Special Magic

--- src/lectures/part1/AdvancedPatternMatching.scala
+++ src/lectures/part1/AdvancedPatternMatching.scala
@@ -2,4 +2,19 @@ package lectures.part1
 
 object AdvancedPatternMatching extends App {
 
+  val numbers = List(1)
+  val description = numbers match {
+    case head :: Nil => println(s"The only element is $head")
+    case _ =>
+  }
+
+  /*
+  Structures available for pattern matching:
+  - Constants
+  - Wildcards
+  - Case Classes
+  - Tuples
+  - Special Magic (like above)
+   */
+
 }
