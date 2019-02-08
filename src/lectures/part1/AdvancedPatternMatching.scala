34813e56840f2f10df21b8a3dd2bb6c6b78dd08d
Rommel Rico <me@rommelrico.com>
2019-02-07 20:52:39 -0800

    Causing a match error

--- src/lectures/part1/AdvancedPatternMatching.scala
+++ src/lectures/part1/AdvancedPatternMatching.scala
@@ -20,13 +20,15 @@ object AdvancedPatternMatching extends App {
   // Making Person class available for Pattern Matching
   class Person(val name: String, val age: Int)
   object Person {
-    def unapply(person: Person): Option[(String, Int)] = Some((person.name, person.age))
+    def unapply(person: Person): Option[(String, Int)] =
+      if (person.age < 21) None
+      else Some((person.name, person.age))
   }
 
-  val bob = new Person(name = "Bob", age = 33)
+  val bob = new Person(name = "Bob", age = 20)
   val greeting = bob match {
-    case Person(n, a) => println(s"Hi, my name is $n and I'm $a years old")
+    case Person(n, a) => s"Hi, my name is $n and I'm $a years old"
   }
-  greeting
+  println(greeting)
 
 }
