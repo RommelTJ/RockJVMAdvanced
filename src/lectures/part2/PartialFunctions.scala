808fccd2a3af123c59750d487b91a642b254c230
Rommel Rico <me@rommelrico.com>
2019-02-09 18:29:52 -0800

    A Fussy Function

--- src/lectures/part2/PartialFunctions.scala
+++ src/lectures/part2/PartialFunctions.scala
@@ -4,4 +4,12 @@ object PartialFunctions extends App {
 
   val aFunction = (x: Int) => x + 1 // Function[Int, Int] === Int => Int
 
+  val aFussyFunction = (x: Int) =>
+    if (x == 1) 42
+    else if (x == 2) 56
+    else if (x == 5) 999
+    else throw new FunctionNotApplicableException
+
+  class FunctionNotApplicableException extends RuntimeException
+  
 }
