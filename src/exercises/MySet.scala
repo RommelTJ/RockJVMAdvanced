fe4b65a80525156016378b4049d85c20f90ab651
Rommel Rico <me@rommelrico.com>
2019-02-10 15:50:12 -0800

    Mapping

--- src/exercises/MySet.scala
+++ src/exercises/MySet.scala
@@ -14,4 +14,5 @@ trait MySet[A] extends (A => Boolean) {
   def contains(elem: A): Boolean
   def +(elem: A): MySet[A]
   def ++(anotherSet: MySet[A]): MySet[A]
+  def map[B](f: A => B): MySet[B]
 }
