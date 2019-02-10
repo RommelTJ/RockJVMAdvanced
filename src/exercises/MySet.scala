379f925ccd379429c5df65b5eb3dffc32a4f1bc2
Rommel Rico <me@rommelrico.com>
2019-02-10 15:56:07 -0800

    Placeholders for EmptySet

--- src/exercises/MySet.scala
+++ src/exercises/MySet.scala
@@ -13,7 +13,7 @@ package exercises
 trait MySet[A] extends (A => Boolean) {
   def apply(elem: A): Boolean =
     contains(elem)
-  
+
   def contains(elem: A): Boolean
   def +(elem: A): MySet[A]
   def ++(anotherSet: MySet[A]): MySet[A]
@@ -24,7 +24,19 @@ trait MySet[A] extends (A => Boolean) {
 }
 
 class EmptySet[A] extends MySet[A] {
+  override def contains(elem: A): Boolean = ???
 
+  override def +(elem: A): MySet[A] = ???
+
+  override def ++(anotherSet: MySet[A]): MySet[A] = ???
+
+  override def map[B](f: A => B): MySet[B] = ???
+
+  override def flatMap[B](f: A => MySet[B]): MySet[B] = ???
+
+  override def filter(predicate: A => Boolean): MySet[A] = ???
+
+  override def foreach(f: A => Unit): Unit = ???
 }
 
-class NonEmptySet[A](head: A, tail: MySet[A]) extends MySet[A]
+class NonEmptySet[A](head: A, tail: MySet[A]) extends MySet[A] 
