9de46eea99593a355a653323b60e4d53bff3f35b
Rommel Rico <me@rommelrico.com>
2019-02-10 15:58:18 -0800

    Implementations for EmptySet

--- src/exercises/MySet.scala
+++ src/exercises/MySet.scala
@@ -24,19 +24,19 @@ trait MySet[A] extends (A => Boolean) {
 }
 
 class EmptySet[A] extends MySet[A] {
-  override def contains(elem: A): Boolean = ???
+  override def contains(elem: A): Boolean = false
 
-  override def +(elem: A): MySet[A] = ???
+  override def +(elem: A): MySet[A] = new NonEmptySet[A](elem, this)
 
-  override def ++(anotherSet: MySet[A]): MySet[A] = ???
+  override def ++(anotherSet: MySet[A]): MySet[A] = anotherSet
 
-  override def map[B](f: A => B): MySet[B] = ???
+  override def map[B](f: A => B): MySet[B] = new EmptySet[B]
 
-  override def flatMap[B](f: A => MySet[B]): MySet[B] = ???
+  override def flatMap[B](f: A => MySet[B]): MySet[B] = new EmptySet[B]
 
-  override def filter(predicate: A => Boolean): MySet[A] = ???
+  override def filter(predicate: A => Boolean): MySet[A] = this
 
-  override def foreach(f: A => Unit): Unit = ???
+  override def foreach(f: A => Unit): Unit = ()
 }
 
 class NonEmptySet[A](head: A, tail: MySet[A]) extends MySet[A] {
