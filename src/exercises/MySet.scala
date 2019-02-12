8087e31c6156f39b08eb1d41e2ed4eb6e327903d
Rommel Rico <me@rommelrico.com>
2019-02-12 06:39:00 -0800

    Implementing Concatenation for ProperyBasedMySet

--- src/exercises/MySet.scala
+++ src/exercises/MySet.scala
@@ -109,8 +109,8 @@ class PropertyBasedMySet[A](property: A => Boolean) extends MySet[A] {
 
   // { x in A | property(x) } + elem = { x in A | property(x) || x == elem }
   override def +(elem: A): MySet[A] = new PropertyBasedMySet[A](x => property(x) || x == elem)
-
-  override def ++(anotherSet: MySet[A]): MySet[A] = this
+  
+  override def ++(anotherSet: MySet[A]): MySet[A] = new PropertyBasedMySet[A](x => property(x) || anotherSet(x))
 
   override def map[B](f: A => B): MySet[B] = ??? // Unimplemented for now
   override def flatMap[B](f: A => MySet[B]): MySet[B] = ???
