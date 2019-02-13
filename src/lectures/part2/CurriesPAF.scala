cd3ae7d4b571487ce3dc9ebe315130073e16ec9a
Rommel Rico <me@rommelrico.com>
2019-02-13 06:39:13 -0800

    Curried functions

--- src/lectures/part2/CurriesPAF.scala
+++ src/lectures/part2/CurriesPAF.scala
@@ -2,4 +2,12 @@ package lectures.part2
 
 object CurriesPAF extends App {
 
+  // Curried Functions
+
+  val superAdder: Int => Int => Int =
+    x => y => x + y
+
+  val add3 = superAdder(3)(2)
+  println(add3)
+
 }
