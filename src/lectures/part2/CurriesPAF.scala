c6829892db52c2303a91a78be0b8ec6c0357015f
Rommel Rico <me@rommelrico.com>
2019-02-13 06:41:56 -0800

    Curried Methods

--- src/lectures/part2/CurriesPAF.scala
+++ src/lectures/part2/CurriesPAF.scala
@@ -4,10 +4,18 @@ object CurriesPAF extends App {
 
   // Curried Functions
 
+  // Function that takes function as an argument.
   val superAdder: Int => Int => Int =
     x => y => x + y
 
   val add3 = superAdder(3)(2)
   println(add3)
+
+  // Curried Methods
+
+  def curriedAdder(x: Int)(y: Int): Int = x + y
+
+  val add4: Int => Int = curriedAdder(4)
+  println(add4(1))
 
 }
