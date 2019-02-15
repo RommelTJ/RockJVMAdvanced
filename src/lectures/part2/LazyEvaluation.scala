f1f4989829e3573870165bb919a7b91674e2e777
Rommel Rico <me@rommelrico.com>
2019-02-14 17:39:04 -0800

    Crash and No Crash

--- src/lectures/part2/LazyEvaluation.scala
+++ src/lectures/part2/LazyEvaluation.scala
@@ -2,4 +2,7 @@ package lectures.part2
 
 object LazyEvaluation extends App {
 
+  // val x: Int = throw new RuntimeException // Crash!
+  lazy val x: Int = throw new RuntimeException // No Crash!
+
 }
