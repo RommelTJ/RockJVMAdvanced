5c7927c92a51b571232b41cedfad499e09a735d7
Rommel Rico <me@rommelrico.com>
2019-02-14 17:45:59 -0800

    Another lazyCondition example

--- src/lectures/part2/LazyEvaluation.scala
+++ src/lectures/part2/LazyEvaluation.scala
@@ -26,5 +26,6 @@ object LazyEvaluation extends App {
   println(if (simpleCondition && lazyCondition) "yes" else "no") // no, but doesn't print "Boolean" because the
   // the lazyCondition is not evaluated unless it's needed and the run time is smart enough to not evaluate it because
   // the first condition will short-circuit!
-
+  println(if (lazyCondition && simpleCondition) "yes" else "no") // no, but prints "Boolean"
+  
 }
