e4bc9d5d45d8065d6793987885f1db89873feecb
Rommel Rico <me@rommelrico.com>
2019-02-14 17:40:54 -0800

    Another example

--- src/lectures/part2/LazyEvaluation.scala
+++ src/lectures/part2/LazyEvaluation.scala
@@ -5,7 +5,13 @@ object LazyEvaluation extends App {
   // Lazy Values are evaluated once, but only when they are used.
   // Lazy delays the evaluation of values.
   // val x: Int = throw new RuntimeException // Crash!
-  lazy val x: Int = throw new RuntimeException // No Crash!
-  println(x) // Now we crash!
+  // lazy val x: Int = throw new RuntimeException // No Crash!
+  // println(x) // Now we crash!
+
+  lazy val x: Int = {
+    println("hello")
+    42
+  }
+  println(x)
 
 }
