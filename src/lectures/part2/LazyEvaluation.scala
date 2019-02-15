b75a930753f2d723d9ea1468618603c5d28eeaac
Rommel Rico <me@rommelrico.com>
2019-02-14 17:45:10 -0800

    lazyCondition

--- src/lectures/part2/LazyEvaluation.scala
+++ src/lectures/part2/LazyEvaluation.scala
@@ -21,4 +21,10 @@ object LazyEvaluation extends App {
     true
   }
   def simpleCondition: Boolean = false
+
+  lazy val lazyCondition = sideEffectCondition
+  println(if (simpleCondition && lazyCondition) "yes" else "no") // no, but doesn't print "Boolean" because the
+  // the lazyCondition is not evaluated unless it's needed and the run time is smart enough to not evaluate it because
+  // the first condition will short-circuit!
+
 }
