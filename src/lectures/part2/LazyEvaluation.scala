c4b94ac4636f483530773b71241a31772c0568e8
Rommel Rico <me@rommelrico.com>
2019-02-14 17:39:39 -0800

    Lazy delays the evaluation of values

--- src/lectures/part2/LazyEvaluation.scala
+++ src/lectures/part2/LazyEvaluation.scala
@@ -2,6 +2,8 @@ package lectures.part2
 
 object LazyEvaluation extends App {
 
+  // Lazy Values are evaluated once, but only when they are used.
+  // Lazy delays the evaluation of values.
   // val x: Int = throw new RuntimeException // Crash!
   lazy val x: Int = throw new RuntimeException // No Crash!
 
