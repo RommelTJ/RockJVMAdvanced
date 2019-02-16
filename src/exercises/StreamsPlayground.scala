58fe2fec56205a8f4d1c2d8206e946a5da49c940
Rommel Rico <me@rommelrico.com>
2019-02-15 20:21:05 -0800

    Moving the exercise to the StreamsPlayground

--- src/exercises/StreamsPlayground.scala
+++ src/exercises/StreamsPlayground.scala
@@ -1,5 +1,36 @@
 package exercises
 
+/*
+  Exercise: Implement a Lazily Evaluated single-linked Stream of elements.
+  Head of stream is always available, but tail is available on demand.
+
+  Ex: naturals = MyStream.from(1)(x => x + 1) = stream of natural numbers (potentially infinite!)
+  Ex: naturals.take(100) // Lazily Evaluated Stream of the first 100 naturals (finite stream)
+  Ex: naturals.take(100).foreach(println) // Prints 100 numbers
+  Ex: naturals.foreach(println) // Should crash - Infinite list
+  Ex: naturals.map(_ * 2) // Stream of all even numbers (potentially infinite)
+   */
+abstract class MyStream[+A] {
+  def isEmpty: Boolean
+  def head: A
+  def tail: MyStream[A]
+
+  def #::[B >: A](element: B): MyStream[B] // Prepend operator
+  def ++[B >: A](anotherStream: MyStream[B]): MyStream[B] // Concatenate streams
+
+  def foreach(f: A => Unit): Unit
+  def map[B](f: A => B): MyStream[B]
+  def flatMap[B](f: A => MyStream[B]): MyStream[B]
+  def filter(predicate: A => Boolean): MyStream[A]
+
+  def take(n: Int): MyStream[A] // Takes the first n elements out of this stream and returns a finite stream of n elements.
+  def takeAsList(n: Int): List[A]
+}
+
+object MyStream {
+  def from[A](start: A)(generator: A => A): MyStream[A] = ???
+}
+
 object StreamsPlayground extends App {
-  
+
 }
--- src/lectures/part2/LazyEvaluation.scala
+++ src/lectures/part2/LazyEvaluation.scala
@@ -75,35 +75,4 @@ object LazyEvaluation extends App {
   } yield a + 1
   List(1, 2, 3).withFilter(_ % 2 == 0).map(_ + 1) // List[Int]
 
-  /*
-  Exercise: Implement a Lazily Evaluated single-linked Stream of elements.
-  Head of stream is always available, but tail is available on demand.
-
-  Ex: naturals = MyStream.from(1)(x => x + 1) = stream of natural numbers (potentially infinite!)
-  Ex: naturals.take(100) // Lazily Evaluated Stream of the first 100 naturals (finite stream)
-  Ex: naturals.take(100).foreach(println) // Prints 100 numbers
-  Ex: naturals.foreach(println) // Should crash - Infinite list
-  Ex: naturals.map(_ * 2) // Stream of all even numbers (potentially infinite)
-   */
-  abstract class MyStream[+A] {
-    def isEmpty: Boolean
-    def head: A
-    def tail: MyStream[A]
-
-    def #::[B >: A](element: B): MyStream[B] // Prepend operator
-    def ++[B >: A](anotherStream: MyStream[B]): MyStream[B] // Concatenate streams
-
-    def foreach(f: A => Unit): Unit
-    def map[B](f: A => B): MyStream[B]
-    def flatMap[B](f: A => MyStream[B]): MyStream[B]
-    def filter(predicate: A => Boolean): MyStream[A]
-
-    def take(n: Int): MyStream[A] // Takes the first n elements out of this stream and returns a finite stream of n elements.
-    def takeAsList(n: Int): List[A]
-  }
-
-  object MyStream {
-    def from[A](start: A)(generator: A => A): MyStream[A] = ???
-  }
-
 }
