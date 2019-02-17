b3a60f2bcf266dd1a141141d580d7bd9720a9300
Rommel Rico <me@rommelrico.com>
2019-02-17 15:30:41 -0800

    flatMap implementation for Success case

--- src/lectures/part2/Monads.scala
+++ src/lectures/part2/Monads.scala
@@ -17,7 +17,12 @@ object Monads extends App {
   }
 
   case class Success[+A](value: A) extends Attempt[A] {
-    override def flatMap[B](f: A => Attempt[B]): Attempt[B] = ???
+    override def flatMap[B](f: A => Attempt[B]): Attempt[B] =
+      try {
+        f(value)
+      } catch {
+        case e: Throwable => Fail(e)
+      }
   }
 
   case class Fail(e: Throwable) extends Attempt[Nothing] {
