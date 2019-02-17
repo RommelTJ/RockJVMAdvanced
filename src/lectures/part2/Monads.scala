c4872ccb8dc88b16fc4ba5d078aa30e140c783c6
Rommel Rico <me@rommelrico.com>
2019-02-17 15:28:59 -0800

    "Unit" or apply implementation of Attempt (Try) Monad

--- src/lectures/part2/Monads.scala
+++ src/lectures/part2/Monads.scala
@@ -3,12 +3,17 @@ package lectures.part2
 object Monads extends App {
 
   // Our Own "Try" Monad.
-  trait Attempt[A] {
+  trait Attempt[+A] {
     def flatMap[B](f: A => Attempt[B]): Attempt[B]
   }
 
   object Attempt {
-    def apply[A](a: => A): Attempt[A] = ???
+    def apply[A](a: => A): Attempt[A] =
+      try {
+        Success(a)
+      } catch {
+        case e: Throwable => Fail(e)
+      }
   }
 
   case class Success[+A](value: A) extends Attempt[A] {
