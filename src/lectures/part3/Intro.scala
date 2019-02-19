e85d91abeca314885a6591490ed80bb186cefee2
Rommel Rico <me@rommelrico.com>
2019-02-18 17:10:37 -0800

    Instancing a thread

--- src/lectures/part3/Intro.scala
+++ src/lectures/part3/Intro.scala
@@ -2,6 +2,13 @@ package lectures.part3
 
 object Intro extends App {
 
+  /*
+  interface Runnable {
+    public void run()
+  }
+   */
   // JVM Threads
-  
+  val aThread = new Thread(new Runnable {
+    override def run(): Unit = ???
+  })
 }
