3bd6c8e849505eeaa1eb22f282ef8dfd249b5ba3
Rommel Rico <me@rommelrico.com>
2019-02-18 17:14:04 -0800

    Common pitfall

--- src/lectures/part3/Intro.scala
+++ src/lectures/part3/Intro.scala
@@ -8,11 +8,13 @@ object Intro extends App {
   }
    */
   // JVM Threads
-  val aThread = new Thread(new Runnable {
+  val runnable = new Runnable {
     override def run(): Unit = println("Running in parallel")
-  })
+  }
+  val aThread = new Thread(runnable)
 
   // Start gives the signal to the JVM to start a JVM thread.
   aThread.start() // Will create a JVM thread, which runs on top of an OS system thread.
+  // runnable.run() // This doesn't do anything in parallel.
 
 }
