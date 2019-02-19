70b7683c2e6b8fa06d238d805d5fbba3fca1fdbd
Rommel Rico <me@rommelrico.com>
2019-02-18 17:12:32 -0800

    JVM Threads

--- src/lectures/part3/Intro.scala
+++ src/lectures/part3/Intro.scala
@@ -11,6 +11,8 @@ object Intro extends App {
   val aThread = new Thread(new Runnable {
     override def run(): Unit = println("Running in parallel")
   })
+
+  // Start gives the signal to the JVM to start a JVM thread.
   aThread.start() // Will create a JVM thread, which runs on top of an OS system thread.
 
 }
