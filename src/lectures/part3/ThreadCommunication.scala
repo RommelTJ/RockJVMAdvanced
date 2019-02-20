76f5655aa41e107253416c83b10a3eb4a83e3f56
Rommel Rico <me@rommelrico.com>
2019-02-20 06:44:23 -0800

    The Producer-Consumer problem

--- src/lectures/part3/ThreadCommunication.scala
+++ src/lectures/part3/ThreadCommunication.scala
@@ -2,4 +2,10 @@ package lectures.part3
 
 object ThreadCommunication extends App {
 
+  /*
+  The Producer-Consumer problem
+  Forcing the thread to run in a particular order (otherwise the process has no value).
+  Producer -> [ x ] -> Consumer
+   */
+
 }
