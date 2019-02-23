0783be2d2e4b56cbbc30ffec6460b81df1e192ee
Rommel Rico <me@rommelrico.com>
2019-02-23 08:45:09 -0800

    Calculating the meaning of life on another thread

--- src/lectures/part3/FuturesPromises.scala
+++ src/lectures/part3/FuturesPromises.scala
@@ -1,5 +1,16 @@
 package lectures.part3
 
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+
 object FuturesPromises extends App {
+
+  def calculateMeaningOfLife: Int = {
+    Thread.sleep(2000)
+    42
+  }
+  val aFuture = Future {
+    calculateMeaningOfLife // calculates the meaning of life on another thread
+  }
 
 }
