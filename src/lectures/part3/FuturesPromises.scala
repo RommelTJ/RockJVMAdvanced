ff74316058eaa19acca8a5f53ae9ace2b716f204
Rommel Rico <me@rommelrico.com>
2019-02-23 08:49:19 -0800

    Waiting on the future

--- src/lectures/part3/FuturesPromises.scala
+++ src/lectures/part3/FuturesPromises.scala
@@ -2,6 +2,7 @@ package lectures.part3
 
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.{Failure, Success}
 
 object FuturesPromises extends App {
 
@@ -12,5 +13,14 @@ object FuturesPromises extends App {
   val aFuture = Future {
     calculateMeaningOfLife // calculates the meaning of life on another thread
   } // compiler adds (global) implicit here.
+
+  // println(aFuture.value) // Option[Try[Int]] --> None
+  println("Waiting on the future")
+  aFuture.onComplete(t => t match {
+    case Success(meaningOfLife) => println(s"the meaning of life is $meaningOfLife")
+    case Failure(exception) => println(s"I failed with $exception")
+  })
+  Thread.sleep(3000)
+  
 
 }
