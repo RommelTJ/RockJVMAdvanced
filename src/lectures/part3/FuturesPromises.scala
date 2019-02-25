258fd0d8295eaa042cb7b875574c7b46b8bd83a8
Rommel Rico <me@rommelrico.com>
2019-02-24 19:08:29 -0800

    Testing

--- src/lectures/part3/FuturesPromises.scala
+++ src/lectures/part3/FuturesPromises.scala
@@ -15,16 +15,16 @@ object FuturesPromises extends App {
   } // compiler adds (global) implicit here.
 
   // println(aFuture.value) // Option[Try[Int]] --> None
-  println("Waiting on the future")
+//  println("Waiting on the future")
 //  aFuture.onComplete(t => t match {
 //    case Success(meaningOfLife) => println(s"the meaning of life is $meaningOfLife")
 //    case Failure(exception) => println(s"I failed with $exception")
 //  })
-  aFuture.onComplete {
-    case Success(meaningOfLife) => println(s"the meaning of life is $meaningOfLife")
-    case Failure(exception) => println(s"I failed with $exception")
-  } // Not necessarily called by the same thread that created this
-  Thread.sleep(3000)
+//  aFuture.onComplete {
+//    case Success(meaningOfLife) => println(s"the meaning of life is $meaningOfLife")
+//    case Failure(exception) => println(s"I failed with $exception")
+//  } // Not necessarily called by the same thread that created this
+//  Thread.sleep(3000)
 
   // Mini Social Network
 
@@ -72,5 +72,7 @@ object FuturesPromises extends App {
       }
     case Failure(e) => e.printStackTrace()
   }
+
+  Thread.sleep(1000)
   
 }
