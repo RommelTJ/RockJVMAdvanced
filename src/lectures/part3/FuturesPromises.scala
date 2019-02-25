30829c69293a4c59213fe682f046e9688f635e9c
Rommel Rico <me@rommelrico.com>
2019-02-25 07:10:45 -0800

    Banking App object

--- src/lectures/part3/FuturesPromises.scala
+++ src/lectures/part3/FuturesPromises.scala
@@ -1,7 +1,8 @@
 package lectures.part3
 
-import scala.concurrent.Future
+import scala.concurrent.{Await, Future}
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration._
 import scala.util.{Failure, Random, Success}
 
 object FuturesPromises extends App {
@@ -104,5 +105,33 @@ object FuturesPromises extends App {
 
   case class User(name: String)
   case class Transaction(sender: String, receiver: String, amount: Double, status: String)
+
+  object BankingApp {
+    val name = "Rommel Banking"
+
+    def fetchUser(name: String): Future[User] = Future {
+      // Simulate fetching from the DB
+      Thread.sleep(500)
+      User(name)
+    }
+
+    def createTransaction(user: User, merchantName: String, amount: Double): Future[Transaction] = Future {
+      // Simulate some processes
+      Thread.sleep(1000)
+      Transaction(user.name, merchantName, amount, "SUCCESS")
+    }
+
+    def purchase(username: String, item: String, merchantName: String, cost: Double): String = {
+      // Fetch the user from the DB
+      // Create a Transaction from the username to the merchantname
+      // WAIT for the transaction to finish.
+      val transactionStatusFuture = for {
+        user <- fetchUser(username)
+        transaction <- createTransaction(user, merchantName, cost)
+      } yield transaction.status
+
+      Await.result(transactionStatusFuture, 2.seconds)
+    }
+  }
 
 }
