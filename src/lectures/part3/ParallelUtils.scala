83620d555f91a41c83ad24095b0db2015bd265e2
Rommel Rico <me@rommelrico.com>
2019-02-27 03:59:57 -0800

    Parallel Collections

--- src/lectures/part3/ParallelUtils.scala
+++ src/lectures/part3/ParallelUtils.scala
@@ -1,5 +1,19 @@
 package lectures.part3
 
+import scala.collection.parallel.immutable.ParVector
+
 object ParallelUtils extends App {
 
+  // 1 - Parallel Collections
+  val parList = List(1, 2, 3).par
+  val parVector = ParVector[Int](1, 2, 3)
+  /*
+  Seq
+  Vector
+  Array
+  Map - Hash, Trie
+  Set - Hash, Trie
+   */
+  // Leads to increased performance (in general)
+  
 }
