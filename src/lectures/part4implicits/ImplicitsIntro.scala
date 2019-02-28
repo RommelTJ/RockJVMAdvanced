9057bfd23467c2f4daddec8b61aa6cbd60d3e45d
Rommel Rico <me@rommelrico.com>
2019-02-28 07:19:50 -0800

    Person class

--- src/lectures/part4implicits/ImplicitsIntro.scala
+++ src/lectures/part4implicits/ImplicitsIntro.scala
@@ -5,4 +5,8 @@ object ImplicitsIntro extends App {
   val pair = "Rommel" -> "619" // How does this compile?
   val intPair = 1 -> 2 // Implicits!
 
+  case class Person(name: String) {
+    def greet = s"Hi, my name is $name!"
+  }
+
 }
