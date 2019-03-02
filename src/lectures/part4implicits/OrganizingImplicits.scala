497c71081ff1e18ca5620dc4b0976ea0dd0d04f2
Rommel Rico <me@rommelrico.com>
2019-03-01 19:48:24 -0800

    ambiguous implicit values

--- src/lectures/part4implicits/OrganizingImplicits.scala
+++ src/lectures/part4implicits/OrganizingImplicits.scala
@@ -4,6 +4,7 @@ object OrganizingImplicits extends App {
 
   // Implicit ordering value for Int in "scala.Predef".
   implicit val reverseOrdering: Ordering[Int] = Ordering.fromLessThan(_ > _)
+  // implicit val normalOrdering: Ordering[Int] = Ordering.fromLessThan(_ > _) // Compilation error because of ambiguous implicit values
   println(List(1, 4, 5, 3, 2).sorted) // and now it's reversed
 
 }
