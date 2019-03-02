180b841c58090ed573bbd92700caa55f0f0fc86a
Rommel Rico <me@rommelrico.com>
2019-03-02 10:45:51 -0800

    Issues with Pattern Matching

--- src/lectures/part4implicits/TypeClasses.scala
+++ src/lectures/part4implicits/TypeClasses.scala
@@ -14,4 +14,17 @@ object TypeClasses extends App {
   // This is only ONE implementation out of quite a number.
   User("Rommel", 30, "rommel@myemail.com").toHTML
 
+  // Option 2 - Pattern Matching
+  // Disadvantages:
+  // Lost type safety
+  // Need to modify code every time
+  // Still only ONE implementation.
+  object HTMLSerializerPatternMatching {
+    def serializeToHTML(value: Any) = value match {
+      case User(n, a, e) =>
+      case java.util.Date =>
+      case _ =>
+    }
+  }
+
 }
