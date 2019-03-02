8a49d801224d1db2d8023762186fdb468852ec13
Rommel Rico <me@rommelrico.com>
2019-03-01 19:47:34 -0800

    Reversing the order

--- src/lectures/part4implicits/OrganizingImplicits.scala
+++ src/lectures/part4implicits/OrganizingImplicits.scala
@@ -3,6 +3,7 @@ package lectures.part4implicits
 object OrganizingImplicits extends App {
 
   // Implicit ordering value for Int in "scala.Predef".
-  println(List(1, 4, 5, 3, 2).sorted)
+  implicit val reverseOrdering: Ordering[Int] = Ordering.fromLessThan(_ > _)
+  println(List(1, 4, 5, 3, 2).sorted) // and now it's reversed
 
 }
