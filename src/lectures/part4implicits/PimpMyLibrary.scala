574408f34fa5389378b50336ab19bdbf3aedb4e4
Rommel Rico <me@rommelrico.com>
2019-03-03 18:43:59 -0800

    Memory optimization purposes

--- src/lectures/part4implicits/PimpMyLibrary.scala
+++ src/lectures/part4implicits/PimpMyLibrary.scala
@@ -3,7 +3,7 @@ package lectures.part4implicits
 object PimpMyLibrary extends App {
 
   // 2.isPrime
-  implicit class RichInt(value: Int) {
+  implicit class RichInt(val value: Int) extends AnyVal {
     def isEven: Boolean = value % 2 == 0
     def squareRoot: Double = Math.sqrt(value)
   }
