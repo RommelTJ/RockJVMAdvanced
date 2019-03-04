c33900115397bd9f6601b038b70570804f229935
Rommel Rico <me@rommelrico.com>
2019-03-03 18:39:34 -0800

    RichInt implicit class

--- src/lectures/part4implicits/PimpMyLibrary.scala
+++ src/lectures/part4implicits/PimpMyLibrary.scala
@@ -2,4 +2,10 @@ package lectures.part4implicits
 
 object PimpMyLibrary extends App {
 
+  // 2.isPrime
+  implicit class RichInt(value: Int) {
+    def isEven: Boolean = value % 2 == 0
+    def squareRoot: Double = Math.sqrt(value)
+  }
+
 }
