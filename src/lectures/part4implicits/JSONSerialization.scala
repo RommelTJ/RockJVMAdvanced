983caba77367f539b8fb4ef3e3bff57a14d4f0cf
Rommel Rico <me@rommelrico.com>
2019-03-05 06:33:34 -0800

    User case class

--- src/lectures/part4implicits/JSONSerialization.scala
+++ src/lectures/part4implicits/JSONSerialization.scala
@@ -2,6 +2,12 @@ package lectures.part4implicits
 
 object JSONSerialization extends App {
 
+  /*
+  Users, Posts, Feeds
+  Serialize to JSON
+   */
+
+  case class User(name: String, age: Int, email: String)
 
 
 }
