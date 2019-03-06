c47118af107de1ce756bb963c7008a4e1947b987
Rommel Rico <me@rommelrico.com>
2019-03-06 06:50:15 -0800

    Actor trait

--- src/lectures/part4implicits/MagnetPattern.scala
+++ src/lectures/part4implicits/MagnetPattern.scala
@@ -9,4 +9,14 @@ object MagnetPattern extends App {
   class P2PResponse
   class Serializer[T]
 
+  trait Actor {
+    def receive(statusCode: Int): Int
+    def receive(request: P2PRequest)
+    def receive(response: P2PResponse)
+    def receive[T: Serializer](message: T): Int
+    def receive[T: Serializer](message: T, statusCode: Int): Int
+    def receive(future: Future[P2PRequest]): Int
+    //... and lots of other overloads
+  }
+
 }
