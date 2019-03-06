1069932a2b23b22d7312264f7c36dd4648989d22
Rommel Rico <me@rommelrico.com>
2019-03-06 06:50:08 -0800

    Some dummy classes

--- src/lectures/part4implicits/MagnetPattern.scala
+++ src/lectures/part4implicits/MagnetPattern.scala
@@ -1,6 +1,12 @@
 package lectures.part4implicits
 
+import scala.concurrent.Future
+
 // Aims solving problems with Method overloading
 object MagnetPattern extends App {
+
+  class P2PRequest
+  class P2PResponse
+  class Serializer[T]
 
 }
