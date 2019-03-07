1dbe845024f28606c2f7b136cab9d59147e963e3
Rommel Rico <me@rommelrico.com>
2019-03-07 06:59:52 -0800

    Converting to a Scala Set

--- src/lectures/part4implicits/ScalaJavaConversions.scala
+++ src/lectures/part4implicits/ScalaJavaConversions.scala
@@ -1,8 +1,15 @@
 package lectures.part4implicits
 
+import java.{util => ju}
+
 object ScalaJavaConversions extends App {
 
   // Object that implicitly converts to Java
   import collection.JavaConverters._
+  val javaSet: ju.Set[Int] = new ju.HashSet[Int]()
+  (1 to 5).foreach(javaSet.add)
+  println(javaSet)
+  val scalaSet = javaSet.asScala // implicit conversion to mutable Set
+
 
 }
