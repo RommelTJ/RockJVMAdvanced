65f8a75591b197fa1f7c0157810a7703c2dcb79b
Rommel Rico <me@rommelrico.com>
2019-03-08 07:05:38 -0800

    Convenience method

--- src/lectures/part5ts/RockingInheritance.scala
+++ src/lectures/part5ts/RockingInheritance.scala
@@ -14,4 +14,11 @@ object RockingInheritance extends App {
     def foreach(f: T => Unit): Unit
   }
 
+  // Convenience method - Whenever you don't know who exactly mixes in our specific traits you can
+  // use them all in a specific type "GenericStream[T] with Writer[T] with Closeable"
+  def processStream[T](stream: GenericStream[T] with Writer[T] with Closeable): Unit = {
+    stream.foreach(println)
+    stream.close(200)
+  }
+
 }
