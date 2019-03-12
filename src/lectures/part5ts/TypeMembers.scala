65cbb21cd4035de2c32de5ec2451946792f523e9
Rommel Rico <me@rommelrico.com>
2019-03-12 06:48:12 -0700

    Examples of Abstract Type Members

--- src/lectures/part5ts/TypeMembers.scala
+++ src/lectures/part5ts/TypeMembers.scala
@@ -6,4 +6,11 @@ object TypeMembers extends App {
   class Dog extends Animal
   class Cat extends Animal
 
+  class AnimalCollection {
+    type AnimalType // abstract type member
+    type BoundedAnimal <: Animal // abstract type member upper-bounded with Animal
+    type SuperBoundedAnimal >: Dog <: Animal // super-bounded on Dog and upper-bounded on Animal
+    type AnimalC = Cat // another name for an existing type. Also an abstract type member
+  }
+
 }
