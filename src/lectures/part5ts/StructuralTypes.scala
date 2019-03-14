43b7218609991ede76debf31a89ab2f96efc5862
Rommel Rico <me@rommelrico.com>
2019-03-14 06:52:11 -0700

    JavaCloseable and HipsterCloseable

--- src/lectures/part5ts/StructuralTypes.scala
+++ src/lectures/part5ts/StructuralTypes.scala
@@ -2,4 +2,11 @@ package lectures.part5ts
 
 object StructuralTypes extends App {
 
+  // structural types
+  type JavaCloseable = java.io.Closeable
+
+  class HipsterCloseable {
+    def close(): Unit = println("yeah yeah I'm closing")
+  }
+
 }
