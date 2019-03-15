5be83853e284ca7881defb6c5202d8aedbf7228b
Rommel Rico <me@rommelrico.com>
2019-03-15 07:00:17 -0700

    Self Types

--- src/lectures/part5ts/SelfTypes.scala
+++ src/lectures/part5ts/SelfTypes.scala
@@ -2,4 +2,15 @@ package lectures.part5ts
 
 object SelfTypes extends App {
 
+  // Self Types are a way of requiring a type to be mixed in.
+
+  trait Instrumentalist {
+    def play(): Unit
+  }
+
+  // Marker at language level that forces whoever implements Singer to implement Instrumentalist
+  trait Singer { self: Instrumentalist =>
+    def sing(): Unit
+  }
+
 }
