fda32c54426df464c93f02a15d108adf18a73330
Rommel Rico <me@rommelrico.com>
2019-03-16 07:23:37 -0700

    Problem example

--- src/lectures/part5ts/FBoundedPolymorphism.scala
+++ src/lectures/part5ts/FBoundedPolymorphism.scala
@@ -2,4 +2,18 @@ package lectures.part5ts
 
 object FBoundedPolymorphism extends App {
 
+  /*
+  trait Animal {
+    def breed(): List[Animal]
+  }
+  class Cat extends Animal {
+    override def breed(): List[Animal] = ??? // Want this to be a List[Cat] !!!
+  }
+  class Dog extends Animal {
+    override def breed(): List[Animal] = ??? // Want this to be a List[Dog] !!!
+  }
+  */
+
+  // How do I make the compiler force the above?
+
 }
