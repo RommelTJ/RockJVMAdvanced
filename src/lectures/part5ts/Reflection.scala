b8a1e8ec6a9309df7d4c57ad9d893cf036cf6316
Rommel Rico <me@rommelrico.com>
2019-03-18 07:04:10 -0700

    How to use the Scala Reflection API

--- src/lectures/part5ts/Reflection.scala
+++ src/lectures/part5ts/Reflection.scala
@@ -12,4 +12,28 @@ object Reflection extends App {
     def sayMyName(): Unit = println(s"Hi, my name is $name.")
   }
 
+  // How to use the Scala Reflection API
+
+  // 0 - import
+  import scala.reflect.runtime.{universe => ru}
+
+  // 1 - Instantiate a MIRROR
+  val m = ru.runtimeMirror(getClass.getClassLoader)
+
+  // 2 - Create a Class object = "description"
+  val clazz = m.staticClass("lectures.part5ts.Reflection.Person") // Creating a class object by NAME.
+
+  // 3 - Create a reflected mirror = "can DO things"
+  val cm = m.reflectClass(clazz)
+
+  // 4 - Get the constructor
+  val constructor = clazz.primaryConstructor.asMethod
+
+  // 5 - Create a reflected constructor
+  val constructorMirror = cm.reflectConstructor(constructor)
+
+  // 6 - Invoke the constructor
+  val instance = constructorMirror.apply("Liza")
+  println(instance) // Person(Liza)
+
 }
