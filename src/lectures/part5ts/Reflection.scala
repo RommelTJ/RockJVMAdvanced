1c4cf0630239dada9793ed438a85389d56371f5e
Rommel Rico <me@rommelrico.com>
2019-03-18 07:09:00 -0700

    How to use the Scala Reflection API to get an instance already created

--- src/lectures/part5ts/Reflection.scala
+++ src/lectures/part5ts/Reflection.scala
@@ -36,4 +36,23 @@ object Reflection extends App {
   val instance = constructorMirror.apply("Liza")
   println(instance) // Person(Liza)
 
+  // Case: I have an instance already computed
+  val p = Person("Mary") // from the "wire" as a serialized object
+  val methodName = "sayMyName" // method name computed from somewhere else
+
+  // 1 - Obtain the mirror
+  // Already done above.
+
+  // 2 - Reflect the instance
+  val reflected = m.reflect(p)
+
+  // 3 - Method Symbol
+  val methodSymbol = ru.typeOf[Person].decl(ru.TermName(methodName)).asMethod
+
+  // 4 - Reflect the Method = "can DO things"
+  val method = reflected.reflectMethod(methodSymbol)
+
+  // 5 - Invoke the method
+  method.apply() // Hi, my name is Mary.
+
 }
