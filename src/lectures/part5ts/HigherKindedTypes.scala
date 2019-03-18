23901297a225a3241e6607b4d50ec2050654a798
Rommel Rico <me@rommelrico.com>
2019-03-17 22:09:04 -0700

    Similar methods 2

--- src/lectures/part5ts/HigherKindedTypes.scala
+++ src/lectures/part5ts/HigherKindedTypes.scala
@@ -1,5 +1,7 @@
 package lectures.part5ts
 
+import scala.concurrent.Future
+
 object HigherKindedTypes extends App {
 
   trait AHigherKindedType[F[_]]
@@ -18,5 +20,23 @@ object HigherKindedTypes extends App {
 
   // Combine/multiply List(1,2) * List("a", "b") => List(1a, 1b, 2a, 2b)
   // etc.
+
+  def multiply[A, B](listA: List[A], listB: List[B]): List[(A, B)] =
+    for {
+      a <- listA
+      b <- listB
+    } yield (a, b)
+
+  def multiply[A, B](listA: Option[A], listB: Option[B]): Option[(A, B)] =
+    for {
+      a <- listA
+      b <- listB
+    } yield (a, b)
+
+  def multiply[A, B](listA: Future[A], listB: Future[B]): Future[(A, B)] =
+    for {
+      a <- listA
+      b <- listB
+    } yield (a, b)
 
 }
