9dfe27ee01571a810b5d31968bd1a6eb04d41bc1
Rommel Rico <me@rommelrico.com>
2019-03-17 22:06:25 -0700

    Similar traits

--- src/lectures/part5ts/HigherKindedTypes.scala
+++ src/lectures/part5ts/HigherKindedTypes.scala
@@ -4,4 +4,18 @@ object HigherKindedTypes extends App {
 
   trait AHigherKindedType[F[_]]
 
+  trait MyList[T] {
+    def flatMap[B](f: T => B): MyList[B]
+  }
+
+  trait MyOption[T] {
+    def flatMap[B](f: T => B): MyOption[B]
+  }
+
+  trait MyFuture[T] {
+    def flatMap[B](f: T => B): MyFuture[B]
+  }
+
+  
+
 }
